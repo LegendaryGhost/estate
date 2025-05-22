d2a972200f935265710590d099c3b7c15c8d4d75
Rihantiana Mbolatsiory <108069670+LegendaryGhost@users.noreply.github.com>
2025-05-22 14:00:57 +0200

    Offer validity

--- models/estate_property_offer.py
+++ models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -9,5 +9,24 @@ class EstatePropertyOffer(models.Model):
         selection=[('accepted', 'Accepted'), ('refused', 'Refused')],
         copy=False
     )
+    validity = fields.Integer(default=7, string="Validity (days)")
+
     partner_id = fields.Many2one("res.partner", required=True)
     property_id = fields.Many2one("estate.property", required=True)
+    
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline", string="Deadline")
+
+    @api.depends("create_date", "validity")
+    def _compute_date_deadline(self):
+        for offer in self:
+            if offer.create_date:
+                offer.date_deadline = fields.Date.add(offer.create_date, days=offer.validity)
+            else:
+                offer.date_deadline = fields.Date.add(fields.Date.today(), days=offer.validity)
+
+    def _inverse_date_deadline(self):
+        for offer in self:
+            if offer.create_date and offer.date_deadline:
+                offer.validity = (offer.date_deadline - fields.Date.to_date(offer.create_date)).days
+            else:
+                offer.validity = 0
